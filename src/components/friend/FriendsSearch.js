9d6192db89c36b2e88211b639b2647a201751a67
jashah46 <jatin.shah01@gmail.com>
2021-03-11 10:06:12 -0600

    Did some unknown changes to pull new branch

---
+++ src/components/friend/FriendsSearch.js
@@ -0,0 +1,54 @@
+import React, { useContext, useState, useEffect } from "react"
+import { FriendsContext } from "./FriendsProvider"
+import { UsersContext } from "../users/UserProvider"
+import { FriendCard } from "./FriendsCard"
+
+
+export const FriendsSearch = () => {
+  const { friends, getFriends } = useContext(FriendsContext)
+  const [filteredUsers, setFilteredFriends] = useState([])
+  const { users, getUsers } = useContext(UsersContext)
+  const [searchTerms, setSearchTerms] = useState("")
+
+    
+  // useEffect(() => {
+  //     getUsers()
+  //     .then(getFriends())
+  // }, [])
+
+  // useEffect(() => {
+  //     if(searchTerms !== ""){
+  //       //   I want to filter users array based on whats in the search terms
+  //       let subSet = users.filter(user => user.name.includes(searchTerms)) 
+  //       // I want to set the state variable to what the .filter returns
+  //       setFilteredFriends(subSet) 
+  //     } else {
+  //       //   what is in the parenthesis is what you are setting filtered users to be 
+  //         (setFilteredFriends([]))
+
+  //     }
+  // }, [searchTerms])
+
+    useEffect(() => {
+        getUsers().then(() => {
+        if (searchTerms !== "") {
+            const subset = users.filter(user => user.name.toLowerCase().includes(searchTerms.toLowerCase()))
+            setFilteredFriends(subset)
+        }
+        })
+    }, [searchTerms])
+
+  return (
+    <>
+      Friend search:
+      <input type="text"
+        className="input--wide"
+        onKeyUp={(event) => setSearchTerms(event.target.value)}
+        placeholder="Search for an Friend... " />
+        {filteredUsers.map(user => <FriendCard key={user.id} friend={user} />)}
+
+        {console.log(filteredUsers)}
+        
+    </>
+  )
+}
