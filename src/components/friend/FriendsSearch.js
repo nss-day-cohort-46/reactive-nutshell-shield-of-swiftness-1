cd000f59faf0bf10aa8d23381b1ec18882db2d1e
Ccourter11 <courtercalvin@yahoo.com>
2021-03-11 14:34:51 -0600

    accidentally merged over all my latest work..

--- src/components/friend/FriendsSearch.js
+++ src/components/friend/FriendsSearch.js
@@ -10,37 +10,25 @@ export const FriendsSearch = () => {
   const { users, getUsers } = useContext(UsersContext)
   const [searchTerms, setSearchTerms] = useState("")
 
-    
-  // useEffect(() => {
-  //     getUsers()
-  //     .then(getFriends())
-  // }, [])
 
-  // useEffect(() => {
-  //     if(searchTerms !== ""){
-  //       //   I want to filter users array based on whats in the search terms
-  //       let subSet = users.filter(user => user.name.includes(searchTerms)) 
-  //       // I want to set the state variable to what the .filter returns
-  //       setFilteredFriends(subSet) 
-  //     } else {
-  //       //   what is in the parenthesis is what you are setting filtered users to be 
-  //         (setFilteredFriends([]))
-
-  //     }
-  // }, [searchTerms])
 
     useEffect(() => {
         getUsers().then(() => {
+          //   I want to filter users array based on whats in the search terms
+          /* check to see if the search bar has something typed into it  */
         if (searchTerms !== "") {
+          /*  if it does, convert it to lowercase letters. convert the user names to lowercase letters. see if the search term is found in the user names */
             const subset = users.filter(user => user.name.toLowerCase().includes(searchTerms.toLowerCase()))
+            // I want to set the state variable to what the .filter returns
             setFilteredFriends(subset)
+            //   what is in the parenthesis is what you are setting filtered users to be 
         }
         })
     }, [searchTerms])
 
   return (
     <>
-      Friend search:
+      <h2>User Search :</h2>
       <input type="text"
         className="input--wide"
         onKeyUp={(event) => setSearchTerms(event.target.value)}
