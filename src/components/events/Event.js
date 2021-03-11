8096aa2982b7829e4a950df546db86e002605d69
emilycentko <emily.centko@gmail.com>
2021-03-11 14:48:08 -0600

    Changed background color of upcoming event

--- src/components/events/Event.js
+++ src/components/events/Event.js
@@ -21,26 +21,28 @@ export const EventCard = ({event, counter}) => {
         deleteEvent(event.id)
     }
 
-    let p = {}
+    let date = {}
+    let location = {}
+    let name = {}
     let section = {}
-    let header = {}
 
     if (counter === 1) {
-        p = {
+        
+        section = {
             fontWeight: 'bold',
-            color: 'blue'
+            backgroundColor: 'lightblue'
         }
     }
     
 
     return (
-        <section className="event">
-            <h5 className="event__name">
+        <section className="event" style={section}>
+            <h5 className="event__name" style={name}>
                 {event.name}
             </h5>
-            <div className="event__date" style={p}>{event.date}
+            <div className="event__date" style={date}>{event.date}
             </div>
-            <div className="event__location">{event.location}</div>
+            <div className="event__location" style={location}>{event.location}</div>
             <button className="event__delete"
             onClick={handleDelete}>Delete Event</button>
 
