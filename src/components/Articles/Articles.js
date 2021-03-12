b254e8c6978b1a834c875090f174022d829c6157
jashah46 <jatin.shah01@gmail.com>
2021-03-12 10:33:45 -0600

    Added delete button

--- src/components/Articles/Articles.js
+++ src/components/Articles/Articles.js
@@ -1,7 +1,15 @@
-import React from "react"
+import React, { useContext } from "react"
+import { ArticleContext } from "./ArticlesProvider"
 // import { Link } from "react-router-dom"
 
 export const ArticleCard = ({ article }) => {
+
+  const { deleteArticle } = useContext(ArticleContext)
+
+  const handleDelete = () => {
+    deleteArticle(article.id)
+  }
+
 return (
   <section className="articles">
     <h3 className="articles__title">
@@ -9,6 +17,8 @@ return (
     </h3>
     <div className="article__url">url: {article.url}</div>
     <div className="article__synopsis">Synopsis: {article.synopsis}</div>
+    <button className="event__delete"
+    onClick={handleDelete}>Delete Article</button>
   </section>
 )
     }
